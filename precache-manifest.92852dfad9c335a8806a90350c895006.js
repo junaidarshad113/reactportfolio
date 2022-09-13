c3de575c7cf9a49356114ace71aa56438e661bb4
unknown <junaidarshad113@gmail.com>
2022-09-12 22:48:19 -0700

    Updates

---
+++ precache-manifest.92852dfad9c335a8806a90350c895006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b5901b62f6adf2f60f643194649031",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "8db2c0777f2c36488662",
+    "url": "/reactportfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/reactportfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/reactportfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db2c0777f2c36488662",
+    "url": "/reactportfolio/static/js/main.2660f066.chunk.js"
+  },
+  {
+    "revision": "ed2a1300382c2d85a850",
+    "url": "/reactportfolio/static/js/runtime-main.bc6e5880.js"
+  }
+]);
